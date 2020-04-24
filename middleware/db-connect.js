c8fe5b53bca9d0fa108b5864d986761515a9c9ef
Ranko95 <ranko.stevanovich@gmail.com>
2020-04-24 15:37:19 +0300

    updates

--- middleware/db-connect.js
+++ middleware/db-connect.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-mongoose.connect('mongodb://localhost:27017/covidApp', {
+mongoose.connect(process.env.DB_CONN, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+})
+  .then(() => console.log('MongoDb connected'))
+  .catch((err) => console.log(err));
 
 module.exports = mongoose.connection;
