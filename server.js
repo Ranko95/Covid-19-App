c6edbaa6f2e913efee690475a3c885e164fb93c4
Ranko95 <ranko.stevanovich@gmail.com>
2020-04-26 14:38:06 +0300

    ws

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
+/* eslint-disable no-restricted-syntax */
 const http = require('http');
+const WebSocketServer = require('ws').Server;
+const moment = require('moment');
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
+
+const connections = new Set();
+
+wss.on('connection', (ws) => {
+  connections.add(ws);
+
+  ws.on('message', (message) => {
+    const time = moment(new Date()).format('HH:mm');
+    const messageData = {
+      time,
+      message,
+    };
+    for (const connection of connections) {
+      connection.send(JSON.stringify(messageData));
+    }
+  });
+
+  ws.on('close', () => {
+    connections.delete(ws);
+  });
+});
 
 server.listen(port, () => console.log(`Listening on ${port}`));
-
-module.exports = {
-  server,
-};
